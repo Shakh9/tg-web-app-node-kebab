a74f9f5e70929e2951d392a7c27d6004d4104d2d
unknown <99ravin99@gmail.com>
2024-06-17 20:18:21 +0500

    Start Beta-test

--- index.js
+++ index.js
@@ -19,53 +19,59 @@ bot.on('message', async msg => {
   const text = msg.text;
 
   if(text === '/start') {
-    await bot.sendMessage(chatId, `Добро пожаловать, ${firstName}! Мы рады приветствовать вас в нашем чат-боте. Для заказа нажмите кнопку "Заказать" снизу.`);
+    await bot.sendMessage(chatId, `Добро пожаловать, ${firstName}! Мы рады приветствовать вас в нашем чат-боте. Для заказа нажмите кнопку "Заказать" снизу.`, {
+      reply_markup: {
+        keyboard: [
+          [{text: 'Заказать', web_app: {url: webAppUrl}}]
+        ]
+      }
+    })
   }
   
-  // if(msg?.web_app_data?.data) {
-  //   try {
-  //     const data = JSON.parse(msg?.web_app_data?.data);
-  //     console.log(data);
-  //     await bot.sendMessage(chatId, `Ваш заказ #0022 принят
-  //     Статус вашего заказа: в обработке
-  //     Сумма заказа: ${data.totalCost}
-  //     Имя: ${data.name}
-  //     Адрес: ${data.address}
-  //     Телефон: ${data.phone}
-  //     Комментарий: ${data.comment}
-  //     Способ доставки: ${data.deliveryMethod}
-  //     Способ оплаты: ${data.paymentMethod}`);
-  //   } catch (e) {
-  //     console.log(e);
-  //   } 
-  // }
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+      await bot.sendMessage(chatId,
+        `Ваш заказ #0022 принят ✅
+Статус вашего заказа: в обработке ⌛
+Сумма заказа: ${data.totalCost}
+Имя: ${data.name}
+Адрес: ${data.address}
+Телефон: ${data.phone}
+Комментарий: ${data.comment}
+Способ доставки: ${data.deliveryMethod}
+Способ оплаты: ${data.paymentMethod}`);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 });
 
-app.post('/web-data', async (req, res) =>  {
-  const {queryId, totalCost, name, address, phone, comment, deliveryMethod, paymentMethod} = req.body;
-  try {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Заказ принят',
-      input_message_content: {
-        message_text: `Поздравляю с покупкой, вы приобрели товар на сумму + ${totalCost}`
-      }
-    })
-    return res.status(200).json({});
-  } catch (e) {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Не удалось оформить заказ',
-      input_message_content: {
-        message_text: 'Не удалось оформить заказ'
-      }
-    })
-    return res.status(500).json({});
-  }
-})
+// app.post('/web-data', async (req, res) =>  {
+//   const {queryId, totalCost, name, address, phone, comment, deliveryMethod, paymentMethod} = req.body;
+//   try {
+//     await bot.answerWebAppQuery(queryId, {
+//       type: 'article',
+//       id: queryId,
+//       title: 'Заказ принят',
+//       input_message_content: {
+//         message_text: `Поздравляю с покупкой, вы приобрели товар на сумму + ${totalCost}`
+//       }
+//     })
+//     return res.status(200).json({});
+//   } catch (e) {
+//     await bot.answerWebAppQuery(queryId, {
+//       type: 'article',
+//       id: queryId,
+//       title: 'Не удалось оформить заказ',
+//       input_message_content: {
+//         message_text: 'Не удалось оформить заказ'
+//       }
+//     })
+//     return res.status(500).json({});
+//   }
+// })
 
-const PORT = 8000;
+// const PORT = 8000;
 
-app.listen(PORT, () => console.log('server started on PORT' + PORT))
+// app.listen(PORT, () => console.log('server started on PORT' + PORT))
